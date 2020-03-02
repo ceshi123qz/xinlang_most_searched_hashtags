6d11264e64c5578d233f516e32c61e8c441ab73f
winterqz <49815962+winterqz@users.noreply.github.com>
2020-03-02 12:42:23 +0800

    Add files via upload
    增加了写入数据库

--- 新浪微博热搜.py
+++ 新浪微博热搜.py
@@ -3,6 +3,7 @@ import requests
 from parsel import Selector
 import csv
 import os
+import pymysql
 
 headers = {
 
@@ -24,26 +25,50 @@ all_info = selector.css('#pl_top_realtimehot > table > tbody > tr')
 # print(all_info)
 current_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
 data = [
-    ('热搜排名', '标题', '热度', '热搜状态', '热搜链接'),
+    ('热搜排名', '标题', '热度', '热搜状态'),
     ('当前时间', current_time, '', '', '')
 ]
+# 连接阿里云服务器数据库
+connection = pymysql.connect(host='*',  # MySQL所在电脑的 ip
+                             port=3306,  # 端口
+                             user='*',  # 用户名
+                             password='*',  # 密码
+                             db='*'  # 数据库名
+                             )  # 字符编码
+
+# print(connection)
+
+cursor = connection.cursor()
 
 for info in all_info:
+
     topic_rank = info.css('tr > td.td-01.ranktop ::text').get()
+    if topic_rank == None:
+        topic_rank = str(0)
+
     topic_title = info.css('tr > td.td-02 > a ::text').get()
     topic_hot_degree = info.css('tr > td.td-02 > span ::text').get()
+    if topic_hot_degree == None:
+        topic_hot_degree = str(0)
     topic_status = info.css('tr > td.td-03 > i ::text').get()
-    topic_link = info.css('tr > td.td-02 > a ::attr(href)').get()
+    # topic_link = info.css('tr > td.td-02 > a ::attr(href)').get()
+    sql = """INSERT INTO wb_hot_rank(`rank`, `hot_degree`, `status`, `title`) VALUES ("%s","%s","%s","%s");""" % (
+        topic_rank, topic_hot_degree, topic_status, topic_title)
+
+    cursor.execute(sql)
+
+    connection.commit()
 
     # print(topic_rank,topic_title,topic_hot_degree,topic_status)
-    each_data = (topic_rank, topic_title, topic_hot_degree, topic_status, 'https://s.weibo.com/' + topic_link)
+    each_data = (topic_rank, topic_title, topic_hot_degree, topic_status)
     data.append(each_data)
 # print(data)
 # timestamp = str(time.time())
-print('正在保存', current_time, '热搜')
+# print('正在保存', current_time, '热搜')
 file_name = '新浪微博热搜' + '.csv'
 with open(file_name, 'a', newline='') as file:
     csv_writer = csv.writer(file)
     for i in data:
         csv_writer.writerow(i)
-print('保存完毕')
+# print('保存完毕')
+connection.close()
